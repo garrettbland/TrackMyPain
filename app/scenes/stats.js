b1b36f6730212a6f71e99faf5ee6d67f6fd9ba86
Garrett <gbland777@gmail.com>
2017-08-01 19:42:58 -0500

    Started on stats

--- app/scenes/stats.js
+++ app/scenes/stats.js
@@ -11,7 +11,8 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import {
   View,
   Text,
-  TouchableOpacity
+  TouchableOpacity,
+  ScrollView
 } from 'react-native';
 
 class Stats extends Component {
@@ -29,9 +30,56 @@ class Stats extends Component {
 
   render(){
     return(
-      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-        <Text>STATS</Text>
+      <View style={{flex:1,}}>
+        <ScrollView>
+          <View style={{width:'100%',height:75,backgroundColor:'white',flexDirection:'row',justifyContent:'space-between',marginTop:30}}>
+            <View style={{width:'70%',paddingLeft:12,justifyContent:'center'}}>
+              <Text style={{fontWeight:'bold',fontSize:22,color:'#34495e',}}>Average Pain for the Last 7 Days</Text>
+            </View>
+            <View style={{paddingRight:12,justifyContent:'center',width:'30%'}}>
+              <View style={{
+                width:90,
+                height:90,
+                alignItems:'center',
+                justifyContent:'center',
+                borderRadius:45,
+                borderColor:this.state.painBackgroundColor ? this.state.painBackgroundColor : '#9b59b6',
+                borderWidth:2,
+                backgroundColor:this.state.painBackgroundColor ? this.state.painBackgroundColor : '#9b59b6',
+                shadowColor: '#000000',
+                shadowOffset: {width: 0,height: 1},
+                shadowRadius: 3,
+                shadowOpacity: 0.7,
+              }}>
+              <Text style={{fontWeight:'bold',fontSize:30,color:'#ffffff'}}>{this.state.pain ? this.state.pain : 0}</Text>
+            </View>
+          </View>
+        </View>
+        <View style={{width:'100%',height:75,backgroundColor:'white',flexDirection:'row',justifyContent:'space-between',marginTop:45}}>
+          <View style={{paddingLeft:12,justifyContent:'center',width:'35%'}}>
+            <View style={{
+              width:90,
+              height:90,
+              alignItems:'center',
+              justifyContent:'center',
+              borderRadius:45,
+              borderColor:this.state.painBackgroundColor ? this.state.painBackgroundColor : '#27ae60',
+              borderWidth:2,
+              backgroundColor:this.state.painBackgroundColor ? this.state.painBackgroundColor : '#27ae60',
+              shadowColor: '#000000',
+              shadowOffset: {width: 0,height: 1},
+              shadowRadius: 3,
+              shadowOpacity: 0.7,
+            }}>
+            <Text style={{fontWeight:'bold',fontSize:30,color:'#ffffff'}}>{this.state.pain ? this.state.pain : 0}</Text>
+          </View>
+        </View>
+        <View style={{width:'65%',paddingRight:12,justifyContent:'center'}}>
+          <Text style={{fontWeight:'bold',fontSize:22,color:'#34495e',}}>Average Pain for the Last 14 Days</Text>
+        </View>
       </View>
+      </ScrollView>
+    </View>
     )
   }
 }

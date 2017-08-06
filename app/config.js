fd76f782f3bbd4fe0bec96bfe423e0066f1c16d5
Garrett <gbland777@gmail.com>
2017-08-06 18:34:30 -0500

    Version 1.0.8 -> 1.0.9

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
-const VERSION = '1.0.8';
+const VERSION = '1.0.9';
 const FIREBASE_API_KEY = 'AIzaSyAFh5LU-FX4lbndFSH8U-hmK_uC3Dtqqos';
 const FIREBASE_AUTH_DOMAIN = 'trackmypain.firebaseapp.com';
 const FIREBASE_DATABASE_URL = 'https://trackmypain.firebaseio.com';
